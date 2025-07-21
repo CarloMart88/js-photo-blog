5f33c035b6db9f81f0fbf9bae99826c75c7c5dc2
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-21 13:18:46 +0200

    feat: 3milestone ok

--- script.js
+++ script.js
@@ -3,18 +3,37 @@ const date = document.getElementById('date')
 const title = document.getElementById('title')
 const immagine = document.getElementById('immagine') 
 
-console.log('awe')
+
 
 const Api = 'https://lanciweb.github.io/demo/api/pictures/'
 
-axios.get(Api).then((resp) => {
-  console.log(resp)
+
+
+
+  axios.get(Api).then((resp) => {
+
+  let result = resp.data
+  console.log(result)
+
+   for(let i = 0 ; i<result.length ; i++){
+  const { title , date , url } = result[i]
+
+  const card = `
+
+     <div class="col-12 col-md-6 col-lg-4">
+          <div class="card position-relative">
+            <img src="./assets1/img/pin.svg" class="position-absolute top-0 start-50 translate-middle">
+            <img src="${url}" class="card-img-top" id="immagine">
+              <div class="card-body">
+                <p class="card-text mono" id="date">${date}</p>
+                <p class="card-text" id="title" >${title}</p>
+              </div>
+          </div>
+        </div>  
+  
+  ` 
+    lista.innerHTML += card}
+ 
 })
 
-const generateCard = () => {
 
-  
-}
-// for(let i = 0 ; i<6 ; i++){
-//   console.log(resp)
-//   }

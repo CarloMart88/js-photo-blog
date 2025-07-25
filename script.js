c820e67cbd97f37112956cbfbbe5e1c3a4e8f84d
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-25 09:58:50 +0200

    feat: add new button and  comments

--- script.js
+++ script.js
@@ -2,26 +2,29 @@ const lista = document.getElementById('lista')
 
 const takeOut = document.querySelector('.none')
 
-const button = document.querySelector('.btn-primary')
+const button = document.querySelector('.button')
 
-
+// dichiaro le variabili 
 
 
 const Api = 'https://lanciweb.github.io/demo/api/pictures/'
 
-
-
+// metto dentro tutto nella chiamata AJAX 
 
   axios.get(Api).then((resp) => {
-
+    
+  // chiamo con una costante il dato della chiamata   
   let result = resp.data
-
+  
+  // vedo il valore 
   console.log(result)
  
 
+ //ciclo l'array di oggetti per inserire in 6 carte i valori della chiamata ottenuti
    for(let i = 0 ; i<result.length ; i++){
   const { title , date , url , id } = result[i]
 
+  // creo la carta 
   const card = `
 
     
@@ -38,15 +41,30 @@ const Api = 'https://lanciweb.github.io/demo/api/pictures/'
         </div>  
   
   ` 
-    lista.innerHTML += card}
+
+    lista.innerHTML += card
+    //inietto la carta nell'html
+  }
+
     
+  // ora che ho generato la chiamata prendo la carta e creo l'id swap 
  
-      const cards = document.querySelectorAll('.immagine')
+      const fotoCompleta = document.querySelectorAll('.immagine')
+      const swap = document.getElementById('swap')
 
-      console.log(cards)
+      console.log(fotoCompleta)
+      console.log(swap)
 
-      cards.forEach((carta , indice ) => {
-        carta.addEventListener('click', () => {
+      // fotoCompleta mi restituisce un array con tutte le immagini
+      // ovviamente se ho un array per interagirvi devo ciclarlo
+      // ad ogni elemento che ho reso cliccabile con il cursor point 
+      //aggiungo l'event 
+      fotoCompleta.forEach((foto ) => {
+        foto.addEventListener('click', () => {
+          console.log(foto)
+          const twins = foto.querySelector('.card-img-top')
+          //assegno a swap.src il valore di twins.src
+          swap.src = twins.src
           takeOut.classList.remove('none');
 
           
@@ -64,4 +82,3 @@ const Api = 'https://lanciweb.github.io/demo/api/pictures/'
         takeOut.classList.add('none')
 
       })
-
